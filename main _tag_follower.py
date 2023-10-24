08c2a4897bd54c439f8395c8b652ca06ed9c24e1
Jack Levitsky <jlevitsky@olin.edu>
2023-10-24 14:25:31 -0400

    Test first image

--- main _tag_follower.py
+++ main _tag_follower.py
@@ -2,8 +2,8 @@ import cv2
 import numpy as np
 from apriltag import apriltag
 
-imagepath = 'test.jpg'
+imagepath = 'test_image.jpg'
 image = cv2.imread(imagepath, cv2.IMREAD_GRAYSCALE)
-detector = apriltag("tagStandard41h12")
+detector = apriltag("tagStandard36h11")
 
 detections = detector.detect(image)
