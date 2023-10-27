2bf5a774bef22964ab5c40ec542dc1c7649c39cb
AJ <ajecoder@gmail.com>
2023-10-27 14:37:50 -0400

    detects multiple tags from image

--- main _tag_follower.py
+++ main _tag_follower.py
@@ -1,9 +1,24 @@
-import cv2
 import numpy as np
-from apriltag import apriltag
+from dt_apriltags import Detector
+import cv2
 
-imagepath = 'test_image.jpg'
+imagepath = 'test_image_tags.jpg'
 image = cv2.imread(imagepath, cv2.IMREAD_GRAYSCALE)
-detector = apriltag("tagStandard36h11")
+detector = Detector("tag36h11")
 
 detections = detector.detect(image)
+
+print(detections)
+cv2.imshow("Greyscale Original",image) # greyscale image
+cv2.waitKey(0)
+
+color_image = cv2.cvtColor(image, cv2.COLOR_GRAY2RGB)
+for detection in detections:
+    for idx in range(len(detection.corners)):
+        cv2.line(color_image, tuple(detection.corners[idx-1, :].astype(int)), tuple(detection.corners[idx, :].astype(int)), (0,255,0), 3)
+cv2.imshow("Tags Detected", color_image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+
+
