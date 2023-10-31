da1c4ce6d8d02abb71d3c95d87fbbb39454e23a0
Jack Levitsky <jlevitsky@olin.edu>
2023-10-31 13:53:36 -0400

    Fixed my horrible mistake

--- main _tag_follower.py
+++ main _tag_follower.py
@@ -1,25 +1,32 @@
 import cv2
 import numpy as np
 from dt_apriltags import Detector
-import cv2
+
+SHOWVISUALS=True
+
 
 imagepath = 'test_image.jpg'
 image = cv2.imread(imagepath, cv2.IMREAD_GRAYSCALE)
-detector = apriltag("tagStandard36h11")
+detector = Detector("tag36h11")
 
 detections = detector.detect(image)
 
-print(detections)
-cv2.imshow("Greyscale Original",image) # greyscale image
-cv2.waitKey(0)
+if SHOWVISUALS==True:
+    cv2.imshow("Greyscale Original",image) # greyscale image
+    k=cv2.waitKey(0)
+    if k==27:
+        cv2.destroyAllWindows
 
 color_image = cv2.cvtColor(image, cv2.COLOR_GRAY2RGB)
 for detection in detections:
     for idx in range(len(detection.corners)):
         cv2.line(color_image, tuple(detection.corners[idx-1, :].astype(int)), tuple(detection.corners[idx, :].astype(int)), (0,255,0), 3)
-cv2.imshow("Tags Detected", color_image)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+
+if SHOWVISUALS==True:
+    cv2.imshow("Tags Detected", color_image)
+    k=cv2.waitKey(0)
+    if k==27:
+        cv2.destroyAllWindows()
 
 
 
